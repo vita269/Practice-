914fc4c52c8035fd19662095b63d8fd1917423bd
Vita Lisitsina <vitalisitsina@mail.ru>
2025-10-18 12:39:46 +0300

    first commit

---
+++ main_test.go
@@ -0,0 +1,17 @@
+package main
+import (
+"testing"
+)
+
+func TestMaxInt(t *testing.T) {
+a,b := 2, 7
+res:= MaxInt(a,b)
+
+if res != b {
+t.Errorf("expected %d, got %d", b, res)
+}
+}
+
+func TestMain(m *testing.M) {
+main()
+}
